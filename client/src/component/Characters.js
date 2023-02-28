4e4db49c99b1b49bae3af77fb72dfe4c02b0083b
Seunggyu <seunggyul14@icloud.com>
2023-02-27 20:53:21 -0800

    search bar in chars

--- client/src/component/Characters.js
+++ client/src/component/Characters.js
@@ -1,19 +1,28 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import Character from './Character'
 import { CharacterContext } from '../Context/CharacterContext';
 
 const Characters = () => {
     const {characters, setCharacters, fetchCharacters} = useContext(CharacterContext)
+    const [searchInput, setSearchInput] = useState('')
 
-    const mappedCharacters = characters.map(character => <Character key={character.id} character={character}/>)
+    const filteredCharacters = characters.filter(char => char.name.toLowerCase().includes(searchInput.toLowerCase()))
+
+    const mappedCharacters = filteredCharacters.map(character => <Character key={character.id} character={character}/>)
+
 
     useEffect(() => {
         fetchCharacters()
     }, [])
 
     return (
-        <div className='chars'>
-            {mappedCharacters}
+        <div className='char_page'>
+            <div className='search'>
+                <input className='search_bar' type='text' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} placeholder='search for...'/>
+            </div>
+            <div className='chars'>
+                {mappedCharacters}
+            </div>
         </div>
     );
 }
